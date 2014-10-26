f2673439d85354176f8a651e8a752e764fdbd251
Raimon Meuldijk <Raimon@phpBB.com>
2014-10-26 19:25:19 +0100

    Paar wijzigingen voor de duidelijkheid

--- language/nl/app.php
+++ language/nl/app.php
@@ -42,7 +42,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'CONTROLLER_ARGUMENT_VALUE_MISSING'	=> 'Missende waarde voor argument #%1$s: <strong>%3$s</strong> in class <strong>%2$s</strong>',
-	'CONTROLLER_NOT_SPECIFIED'			=> 'Er is geen controller gespecificeerd.',
-	'CONTROLLER_METHOD_NOT_SPECIFIED'	=> 'Er is geen methode gespecificeerd voor de controller.',
+	'CONTROLLER_NOT_SPECIFIED'			=> 'Er is geen controller opgegeven.',
+	'CONTROLLER_METHOD_NOT_SPECIFIED'	=> 'Er is geen methode opgegeven voor de controller.',
 	'CONTROLLER_SERVICE_UNDEFINED'		=> 'De service voor controller "<strong>%s</strong>" is niet gedefinieerd in ./config/services.yml.',
 ));
