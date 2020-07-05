f2e66e448a7b0a341d1ed34545c7c3bf03797db9
Jim Mossing Holsteyn <jim@beta-garden.com>
2020-07-05 16:50:29 +0200

    3.3.1-RC1 wijzigingen

--- language/nl/captcha_recaptcha.php
+++ language/nl/captcha_recaptcha.php
@@ -37,16 +37,38 @@ if (empty($lang) || !is_array($lang))
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
 
 $lang = array_merge($lang, array(
-	'RECAPTCHA_LANG'				=> 'nl', 	// Find the language/country code on https://developers.google.com/recaptcha/docs/language - If no code exists for your language you can use "en" or leave the string empty
-	'RECAPTCHA_NOT_AVAILABLE'		=> 'Om gebruik te kunnen maken van reCaptcha, moet je een account aanmaken op <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
-	'CAPTCHA_RECAPTCHA'				=> 'reCaptcha',
-	'RECAPTCHA_INCORRECT'			=> 'De oplossing die je hebt ingevoerd is onjuist',
+	// Find the language/country code on https://developers.google.com/recaptcha/docs/language
+	// If no code exists for your language you can use "en" or leave the string empty
+	'RECAPTCHA_LANG'				=> 'nl',
+
+	'CAPTCHA_RECAPTCHA'				=> 'reCaptcha v2',
+	'CAPTCHA_RECAPTCHA_V3'			=> 'reCaptcha v3',
 	'RECAPTCHA_NOSCRIPT'			=> 'Schakel JavaScript in om de uitdaging te kunnen laden.',
+	'RECAPTCHA_NOT_AVAILABLE'		=> 'Om gebruik te kunnen maken van reCaptcha, moet je een account aanmaken op <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
+	'RECAPTCHA_INVISIBLE'			=> 'Deze CAPTCHA is onzichtbaar. Wanneer deze CAPTCHA werkt, staat er een klein icoon in de rechterbenedenhoek van deze pagina.',
 
 	'RECAPTCHA_PUBLIC'				=> 'Site sleutel',
-	'RECAPTCHA_PUBLIC_EXPLAIN'		=> 'Je reCaptcha site-sleutel. Sleutels kunnen verkregen worden via <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>. Gebruik hiervoor reCAPTCHA v2 &gt; Invisible reCAPTCHA badge als type.',
+	'RECAPTCHA_PUBLIC_EXPLAIN'		=> 'Je reCaptcha site-sleutel. Sleutels kunnen verkregen worden via <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>. Gebruik hiervoor reCAPTCHA v2 &gt; Invisible reCAPTCHA badge als type.',
+	'RECAPTCHA_V3_PUBLIC_EXPLAIN'		=> 'Je reCaptcha site-sleutel. Sleutels kunnen verkregen worden via <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>. Gebruik hiervoor reCAPTCHA v3.',
 	'RECAPTCHA_PRIVATE'				=> 'Privésleutel',
-	'RECAPTCHA_PRIVATE_EXPLAIN'		=> 'Je reCaptcha privé-sleutel. Sleutels kunnen verkregen worden via <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>. Gebruik hiervoor reCAPTCHA v2 &gt; Invisible reCAPTCHA badge als type.',
+	'RECAPTCHA_PRIVATE_EXPLAIN'		=> 'Je reCaptcha privé-sleutel. Sleutels kunnen verkregen worden via <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>. Gebruik hiervoor reCAPTCHA v2 &gt; Invisible reCAPTCHA badge als type.',
+	'RECAPTCHA_V3_PRIVATE_EXPLAIN'		=> 'Je reCaptcha privé-sleutel. Sleutels kunnen verkregen worden via <a href="https://www.google.com/recaptcha">www.google.com/recaptcha</a>. Gebruik hiervoor reCAPTCHA v3.',
 
-	'RECAPTCHA_INVISIBLE'			=> 'Deze CAPTCHA is onzichtbaar. Wanneer deze CAPTCHA werkt, staat er een klein icoon in de rechterbenedenhoek van deze pagina.',
+	'RECAPTCHA_V3_DOMAIN'				=> 'Requestdomein',
+	'RECAPTCHA_V3_DOMAIN_EXPLAIN'		=> 'Domein waar het script om het request te verifieren vandaan moet komen.<br>Gebruik <samp>recaptcha.net</samp> als <samp>google.com</samp> niet beschikbaar is.',
+
+	'RECAPTCHA_V3_METHOD'				=> 'Requestmethode',
+	'RECAPTCHA_V3_METHOD_EXPLAIN'		=> 'De methode die gebruikt moet worden om het request te verifieren.<br>Niet ingeschakelde opties op jouw domein worden niet weergegeven.',
+	'RECAPTCHA_V3_METHOD_CURL'			=> 'cURL',
+	'RECAPTCHA_V3_METHOD_POST'			=> 'POST',
+	'RECAPTCHA_V3_METHOD_SOCKET'		=> 'Socket',
+
+	'RECAPTCHA_V3_THRESHOLD_DEFAULT'			=> 'Standaard maximumscore',
+	'RECAPTCHA_V3_THRESHOLD_DEFAULT_EXPLAIN'	=> 'Wordt gebruikt wanneer andere opties niet beschikbaar zijn.',
+	'RECAPTCHA_V3_THRESHOLD_LOGIN'				=> 'Login maximumscore',
+	'RECAPTCHA_V3_THRESHOLD_POST'				=> 'Berichten maximumscore',
+	'RECAPTCHA_V3_THRESHOLD_REGISTER'			=> 'Registratie maximumscore',
+	'RECAPTCHA_V3_THRESHOLD_REPORT'				=> 'Rapporteren maximumscore',
+	'RECAPTCHA_V3_THRESHOLDS'					=> 'Maximumscores',
+	'RECAPTCHA_V3_THRESHOLDS_EXPLAIN'			=> 'reCAPTCHA v3 geeft een score weer (<samp>1.0</samp> is zeer waarschijnlijk een normale interactie, <samp>0.0</samp> is zeer waarschijnlijk een bot). Hier kan je de maximumscore voor elke actie instellen',
 ));
