f4357677ead6982e4a42c361d4016298cc2f8f6f
Derk <derky@phpbb.com>
2026-04-29 21:38:43 +0200

    Fix typo and revise HTTP authentication explanation

--- language/nl/acp/board.php
+++ language/nl/acp/board.php
@@ -298,7 +298,7 @@ $lang = array_merge($lang, array(
 	'ACP_FEED_OVERALL_FORUMS_EXPLAIN'	=> 'Schakelt de “Alle forums”-feed in, welke een lijst van forums weergeeft.',
 
 	'ACP_FEED_HTTP_AUTH'				=> 'HTTP-Authenticatie toestaan',
-	'ACP_FEED_HTTP_AUTH_EXPLAIN'		=> 'Schakelt HTTP-authenticatie in, welke toestaat gebruikers inhoud te ontvangen die verborgen is voor gasten door het toevoegen van de <samp>auth=http</samp> parameter aan de feed URL. Let op dat sommige PHP installaties extra wijzigingen vereisen aan het .htaccess bestand. Instructies kunnen gevonden worden in dat bestand. Let op dat HTTP authentication alleen onderteund wordt over een beveiligde verrbinding.',
+	'ACP_FEED_HTTP_AUTH_EXPLAIN'		=> 'Schakelt HTTP-authenticatie in, waardoor gebruikers toegang kunnen krijgen tot inhoud die voor gasten verborgen is door de <samp>auth=http</samp>-parameter aan de feed-URL toe te voegen. Let op dat sommige PHP installaties extra wijzigingen vereisen aan het .htaccess bestand; raadpleeg dat bestand voor meer informatie. HTTP-authenticatie wordt alleen ondersteund via versleutelde (HTTPS) verbindingen.',
 
 	'ACP_FEED_ITEM_STATISTICS'			=> 'Item statistieken',
 	'ACP_FEED_ITEM_STATISTICS_EXPLAIN'	=> 'Geeft individuele statistieken weer onder de feed-items<br>(bijv. geplaatst door, datum en tijd, reacties, weergaves)',
